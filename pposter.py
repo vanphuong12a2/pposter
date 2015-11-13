13401896c32c89894a34816db81b4272e9cd8773
phuong.nguyen <phuongnguyen@hdeadmins-MacBook-Air-3.local>
2015-11-13 17:24:51 +0900

    Add register + support multi users from register

--- pposter.py
+++ pposter.py
@@ -8,9 +8,8 @@ Date: 10 Nov 2015
 """
 
 import os
-from flask import Flask, g, url_for, render_template, request, redirect, flash, session
+from flask import Flask, g, url_for, render_template, request, redirect, flash, session, abort
 import redis
-import time
 from oauth2client.client import flow_from_clientsecrets
 import httplib2
 from apiclient.discovery import build
@@ -40,22 +39,19 @@ model = RedisModel(r, conn, app.config)
 @app.before_request
 def before_request():
     g.test = app.config['TEST']
-
-
-@app.route('/')
-def do_nothing():
-    if 'logged_in' in session and session['logged_in']:
-        return redirect(url_for('timeline'))
-    else:
-        return render_template('layout.html')
+    g.curr_user = None
+    if 'user_id' in session:
+        g.curr_user = (model.get_username(session['user_id']), model.get_userlink(session['user_id']))
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if g.curr_user is not None:
+        return redirect(url_for('timeline'))
     error = None
     if request.method == 'POST':
-        if (app.config['USERNAME'], app.config['PASSWORD']) == (request.form['username'], request.form['password']):
-            session['logged_in'] = True
+        if model.is_valid_user(request.form['email'], request.form['password']):
+            session['user_id'] = request.form['email']
             flash("You were logged in!")
             return redirect(url_for('timeline'))
         else:
@@ -63,8 +59,33 @@ def login():
     return render_template('login.html', error=error)
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if g.curr_user is not None:
+        return redirect(url_for('timeline'))
+    error = None
+    if request.method == 'POST':
+        if not request.form['email'] or '@' not in request.form['email']:
+            error = 'You have to enter an valid email'
+        elif model.is_registered(request.form['email']):
+            error = 'This email was registered'
+        elif not request.form['name']:
+            error = 'You have to enter your name'
+        elif not request.form['password']:
+            error = 'You have to enter a password'
+        elif request.form['password'] != request.form['password2']:
+            error = 'The two passwords do not match'
+        else:
+            model.add_user(request.form['email'], request.form['name'], request.form['password'])
+            flash('You were successfully registered and can login now')
+            return redirect(url_for('login'))
+    return render_template('register.html', error=error)
+
+
 @app.route('/google_auth')
 def google_auth():
+    if g.curr_user is not None:
+        return redirect(url_for('timeline'))
     flow = flow_from_clientsecrets(app.config['GCLIENT_SECRETS'], scope='profile', redirect_uri=url_for('auth_return', _external=True))
     auth_uri = flow.step1_get_authorize_url()
     return redirect(auth_uri)
@@ -72,6 +93,8 @@ def google_auth():
 
 @app.route('/auth_return', methods=['GET'])
 def auth_return():
+    if g.curr_user is not None:
+        return redirect(url_for('timeline'))
     flow = flow_from_clientsecrets(app.config['GCLIENT_SECRETS'], scope='profile', redirect_uri=url_for('auth_return', _external=True))
     if 'code' in request.args:
         code = request.args.get('code')
@@ -81,7 +104,7 @@ def auth_return():
         service = build('plus', 'v1', http=http)
         user_info = service.people().get(userId='me').execute()
         if user_info['id'] == app.config['GOOGLE_ID']:
-            session['logged_in'] = True
+            session['user_id'] = user_info['email']  # Need to be checked
             flash("You were logged in!")
             return redirect(url_for('timeline'))
         else:
@@ -92,29 +115,75 @@ def auth_return():
 
 @app.route('/logout')
 def logout():
-    session.pop('logged_in', None)
+    session.pop('user_id', None)
     flash('You were logged out!')
-    return redirect(url_for('do_nothing'))
+    return redirect(url_for('timeline'))
 
 
-@app.route('/timeline')
+@app.route('/', methods=['GET'])
 def timeline():
-    tweets = model.get_tweets(0)
-    return render_template('timeline.html', tweets=tweets)
+    if g.curr_user is not None:
+        lusers = None
+        tweets = model.get_tweets(lusers=lusers, offset=0)
+        return render_template('timeline.html', tweets=tweets)
+    else:
+        return render_template('layout.html')
 
 
 @app.route('/timelinejson', methods=['GET'])
 def timelinejson():
+    if g.curr_user is None:
+        return render_template('login.html')
     if 'offset' in request.args:
         offset = int(request.args['offset'])
-        tweets = model.get_tweets(offset)
+        tweets = model.get_tweets(offset=offset)
         return json.dumps(tweets)
     else:
-        return "No offset!"
+        abort(404)
+
+
+@app.route('/<userlink>/timelinejson', methods=['GET'])
+def user_timelinejson(userlink):
+    if g.curr_user is None:
+        return render_template('login.html')
+    if 'offset' in request.args:
+        uid = model.get_userid(userlink)
+        if model.registered(uid):
+            offset = int(request.args['offset'])
+            tweets = model.get_tweets(lusers=[uid], offset=offset)
+            return json.dumps(tweets)
+        else:
+            return redirect(url_for('user_timeline', userlink=g.curr_user[1]))
+    else:
+        abort(404)
+
+
+@app.route('/<userlink>')
+def user_timeline(userlink):
+    if g.curr_user is None:
+        return render_template('login.html')
+    uid = model.get_userid(userlink)
+    if model.is_registered(uid):
+        tweets = model.get_tweets(lusers=[uid], offset=0)
+        return render_template('timeline.html', tweets=tweets)
+    else:
+        flash("There is no user with that id")
+        return redirect(url_for('timeline'))
+
+
+@app.route('/public')
+def public_timeline():
+    tweets = model.get_tweets(offset=0)
+    return render_template('timeline.html', tweets=tweets)
 
 
 @app.route('/add_tweet', methods=['POST'])
-def add_tweet():
+@app.route('/<userlink>/add_tweet', methods=['POST'])
+def add_tweet(userlink=None):
+    if g.curr_user is None:
+        return render_template('login.html')
+    if userlink is not None and userlink != g.curr_user[1]:
+        return "Illegal post"
     error = None
     if request.method == 'POST':
         #Get tweet
@@ -137,8 +206,11 @@ def add_tweet():
                 tweet_file.save(os.path.join(app.config['UPLOAD_FOLDER'], new_imgname))
             else:
                 model.s3_put(conn, app.config['BUCKET'], new_imgname, tweet_file)
-        model.add_tweet(tweet_content, time.time(), 'user', new_imgname)
-        return redirect(url_for('timeline'))
+        model.add_tweet(tweet_content, session['user_id'], new_imgname)
+        if userlink is None:
+            return redirect(url_for('timeline'))
+        else:
+            return redirect(url_for('user_timeline', userlink=userlink))
 
 if __name__ == '__main__':
     if app.config['TEST']:
--- redis_model.py
+++ redis_model.py
@@ -1,23 +1,27 @@
 import base64
 import os
 import common
+import time
 
-new_tweet_id = 'new_tweet_id'
-tweets = 'tweet_ids'
-tweet_content = 'tweet_content'
-tweet_img = 'tweet_img'
-tweet_time = 'tweet_time'
-tweet_user = 'tweet_user'
+NEW_TWEET_ID = 'new_tweet_id'
+TWEETS = 'tweet_ids'
+TWEET_CONTENT = 'tweet_content'
+TWEET_IMG = 'tweet_img'
+TWEET_TIME = 'tweet_time'
+TWEET_USER = 'tweet_user'
 
-users = 'user_emails'
-user_firstname = 'user_firstname'
-user_lastname = 'user_lastname'
-user_img = 'user_img'
-user_joined = 'user_joined'
-user_tweets = 'user_tweets'
-user_followings = 'user_followings'
-user_followeds = 'user_followed'
-user_pass = 'user_pass'
+USERS = 'user_emails'
+USER_NAME = 'user_name'
+USER_IMG = 'user_img'
+USER_JOINED = 'user_joined'
+USER_LINK = 'user_link'
+USER_TWEETS = 'user_tweets'
+USER_FOLLOWINGS = 'user_followings'
+USER_FOLLOWEDS = 'user_followed'
+USER_PASS = 'user_pass'
+
+HUSERLINK = 'userlink_id_hash'
+NEW_USERLINK = 'new_userlink'
 
 
 def get_tweet_hkey(tweet_id):
@@ -46,42 +50,84 @@ class RedisModel(object):
         self.conn.Object(bucket, filename).delete()
 
     def set_new_tweet_id(self):
-        if self.r.get(new_tweet_id) is None:
-            self.r.set(new_tweet_id, 0)
-        return self.r.incr(new_tweet_id)
+        if self.r.get(NEW_TWEET_ID) is None:
+            self.r.set(NEW_TWEET_ID, 0)
+        return self.r.incr(NEW_TWEET_ID)
 
     def get_new_tweet_id(self):
-        if self.r.get(new_tweet_id) is None:
+        if self.r.get(NEW_TWEET_ID) is None:
             return 1
-        return self.r.get(new_tweet_id)
+        return self.r.get(NEW_TWEET_ID)
 
-    def add_tweet(self, content, time, user, img=None):
+    def set_new_userlink(self):
+        if self.r.get(NEW_USERLINK) is None:
+            self.r.set(NEW_USERLINK, 1000)
+        return "user" + str(self.r.incr(NEW_USERLINK))
+
+    def add_tweet(self, content, user, img=None):
         tweet_id = self.set_new_tweet_id()
+        ttime = time.time()
+        print content, user
         if img is not None:
-            self.r.hmset(get_tweet_hkey(tweet_id), {tweet_content: content, tweet_time: time, tweet_user: user, tweet_img: img})
+            self.r.hmset(get_tweet_hkey(tweet_id), {TWEET_CONTENT: content, TWEET_TIME: ttime, TWEET_USER: user, TWEET_IMG: img})
         else:
-            self.r.hmset(get_tweet_hkey(tweet_id), {tweet_content: content, tweet_time: time, tweet_user: user})
-        self.r.lpush(tweets, tweet_id)
+            self.r.hmset(get_tweet_hkey(tweet_id), {TWEET_CONTENT: content, TWEET_TIME: ttime, TWEET_USER: user})
+        self.r.lpush(TWEETS, tweet_id)
         return tweet_id
 
-    def get_tweets(self, offset):
+    def get_tweet(self, tweet_id):
+        tname = get_tweet_hkey(tweet_id)
+        hkeys = self.r.hkeys(tname)
+        tweet = {}
+        for k in hkeys:
+            val = self.r.hmget(tname, k)[0]
+            if k == TWEET_IMG and val:
+                if self.config['TEST']:
+                    tweet[k] = os.path.join('tmp', val)
+                else:
+                    tweet[k] = self.s3_get(self.config['BUCKET'], val)
+            elif k == TWEET_TIME:
+                tweet[k] = common.format_datetime(float(val))
+            else:
+                tweet[k] = unicode(val, "utf8")
+            if k == TWEET_USER:
+                tweet['username'] = self.get_username(val)
+                tweet['userlink'] = self.get_userlink(val)
+        return tweet
+
+    def get_tweets(self, lusers=None, offset=None):
         twits = []
-        if offset < self.r.llen(tweets):
-            tweet_ids = self.r.lrange(tweets, offset, min(offset + self.config['TWEETS_PER_PAGE'] - 1, self.r.llen(tweets)))
-            for tid in tweet_ids:
-                tname = get_tweet_hkey(tid)
-                hkeys = self.r.hkeys(tname)
-                tweet = {}
-                for k in hkeys:
-                    val = self.r.hmget(tname, k)[0]
-                    if k == tweet_img and val:
-                        if self.config['TEST']:
-                            tweet[k] = os.path.join('tmp', val)
-                        else:
-                            tweet[k] = self.s3_get(self.config['BUCKET'], val)
-                    elif k == tweet_time:
-                        tweet[k] = common.format_datetime(float(val))
-                    else:
-                        tweet[k] = unicode(val, "utf8")
-                twits.append(tweet)
+        tweet_ids = self.r.lrange(TWEETS, 0, -1)
+        if lusers is not None:
+            tweet_ids = [tid for tid in tweet_ids if self.r.hmget(get_tweet_hkey(tid), TWEET_USER)[0] in lusers]
+        if offset is not None:
+            offset_end = offset + self.config['TWEETS_PER_PAGE']
+            tweet_ids = tweet_ids[offset:offset_end]
+        for tid in tweet_ids:
+            tweet = self.get_tweet(tid)
+            twits.append(tweet)
         return twits
+
+    def is_registered(self, email):
+        return email in self.r.lrange(USERS, 0, -1)
+
+    def add_user(self, email, name, password, img=None):
+        joined = time.time()
+        userlink = self.set_new_userlink()
+        self.r.hmset(get_user_hkey(email), {USER_NAME: name, USER_PASS: password, USER_JOINED: joined, USER_LINK: userlink})
+        self.r.lpush(USERS, email)
+        self.r.hset(HUSERLINK, userlink, email)
+        return email
+
+    def is_valid_user(self, email, password):
+        stored_pass = self.r.hmget(get_user_hkey(email), USER_PASS)[0]
+        return stored_pass == password
+
+    def get_username(self, uid):
+        return self.r.hmget(get_user_hkey(uid), USER_NAME)[0]
+
+    def get_userid(self, userlink):
+        return self.r.hget(HUSERLINK, userlink)
+
+    def get_userlink(self, uid):
+        return self.r.hmget(get_user_hkey(uid), USER_LINK)[0]
